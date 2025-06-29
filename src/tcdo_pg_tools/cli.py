810ebf336d018549245360e9ca281885537245d0
apsteinberg <asherps@gmail.com>
2025-06-29 11:16:16 -0400

    option to display version number

--- src/tcdo_pg_tools/cli.py
+++ src/tcdo_pg_tools/cli.py
@@ -2,8 +2,11 @@ import click
 from tcdo_pg_tools.fusion_merge import fusion_merge
 from tcdo_pg_tools.coverage_calculator import coverage_calculator
 from tcdo_pg_tools.merge_proteome import merge_pg_results, merge_fasta
+import importlib.metadata
 
 @click.group()
+@click.version_option(version=importlib.metadata.version('tcdo-pg-tools'),
+                      message="%(version)s")
 def cli():
     pass
 
